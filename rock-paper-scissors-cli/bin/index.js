8dc8b256fd4cd284abdb968fe4fd13c96e3b3b5c
Toni Muthwa <katheuantona0@gmail.com>
2026-05-17 23:17:05 +0300

    Delete rock-paper-scissors-cli directory

--- rock-paper-scissors-cli/bin/index.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import { program } from 'commander';
-import gameState from '../src/lib/state.js';
-import { showMainMenu } from '../src/lib/gameLogic.js';
-
-showMainMenu(gameState);
-program.parse(process.argv);
